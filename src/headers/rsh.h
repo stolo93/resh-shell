103fc843e5492c578e583d8460b7ea17a3e46ad8
Samuel <samuelstolko@gmail.com>
2022-06-01 01:00:00 +0200

    added main C file, header and Makefile

---
+++ src/headers/rsh.h
@@ -0,0 +1,16 @@
+/**
+ * @file rsh.h
+ * @author Samuel Stolarik (stud.fit.vutbr.cz)
+ * @brief rsh shell API
+ * @version 0.1
+ * @date 2022-05-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef _RSH_H
+#define _RSH_h
+
+
+#endif //_RSH_H
